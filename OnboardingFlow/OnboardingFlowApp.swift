53025e99d7cf7e1cc7e0bc00ce0fb501d4d3dba6
hillarynda <hillarynda0@gmail.com>
2023-11-19 16:58:03 -0300

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Apple Dev on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
